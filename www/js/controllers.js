10a5791c47e1d0540512a72fda9d53feb71a57b3
Cody Derrick <cderrick@tradestation.com>
2015-07-24 15:40:04 -0500

    updated charts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -68,69 +68,83 @@ angular.module('starter.controllers', ['tc.chartjs'])
           pointStrokeColor: '#fff',
           pointHighlightFill: '#fff',
           pointHighlightStroke: 'rgba(220,220,220,1)',
-          data: [80.1, 80.9, 81.2, 81.2, 81.1, 80.9, 79.8]
+          data: [80.1, 80.9, 81.2, 80.2, 81.1, 80.9, 79.8]
+        }
+      ]
+    };
+    
+    $scope.phData = {
+      labels: GetDates(d1, 6),
+      datasets: [
+        {
+          label: 'This Week',
+          fillColor: 'rgba(220,220,220,0.2)',
+          strokeColor: 'rgba(220,220,220,1)',
+          pointColor: 'rgba(220,220,220,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data: [8.1, 7.9, 8.2, 8.2, 8.1, 8.0, 8.2]
+        }
+      ]
+    };
+    
+    $scope.salinityData = {
+      labels: GetDates(d1, 6),
+      datasets: [
+        {
+          label: 'This Week',
+          fillColor: 'rgba(220,220,220,0.2)',
+          strokeColor: 'rgba(220,220,220,1)',
+          pointColor: 'rgba(220,220,220,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data: [1.026, 1.025, 1.025, 1.026, 1.024, 1.025, 1.026]
         }
       ]
     };
     
     $scope.options =  {
-
       // Sets the chart to be responsive
       responsive: true,
-        
-     scaleShowLabels: false,
-
+      scaleShowLabels: false,
       ///Boolean - Whether grid lines are shown across the chart
       scaleShowGridLines : true,
-
       //String - Colour of the grid lines
       scaleGridLineColor : "rgba(0,0,0,.05)",
-
       //Number - Width of the grid lines
       scaleGridLineWidth : 1,
-
       //Boolean - Whether the line is curved between points
       bezierCurve : false,
-
       //Boolean - Whether to show a dot for each point
       pointDot : true,
-
       //Number - Radius of each point dot in pixels
       pointDotRadius : 4,
-
       //Number - Pixel width of point dot stroke
       pointDotStrokeWidth : 1,
-
       //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
       pointHitDetectionRadius : 20,
-
       //Boolean - Whether to show a stroke for datasets
       datasetStroke : true,
-
       //Number - Pixel width of dataset stroke
       datasetStrokeWidth : 2,
-
       //Boolean - Whether to fill the dataset with a colour
       datasetFill : true,
-
       // Function - on animation progress
       onAnimationProgress: function(){},
-
       // Function - on animation complete
       onAnimationComplete: function(){},
-
       //String - A legend template
       legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
     };
-    
-
     
     function GetDates(startDate, daysToGoBack) {
         var aryDates = [];
         for (var i = daysToGoBack; i >= 0; i--) {
             var currentDate = new Date();
             currentDate.setDate(startDate.getDate() - i);
-            aryDates.push(moment(currentDate).format('dd'));
+            aryDates.push(moment(currentDate).format('ddd'));
         }
         return aryDates;
     }
