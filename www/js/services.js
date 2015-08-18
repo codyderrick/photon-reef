384a5e38e9389fa429888d61083aad552ce09560
Cody Derrick <cderrick@tradestation.com>
2015-08-18 14:36:11 -0500

    Changed data store to use connect.io
    sparkfun wasn't cutting it. checking out connect.io

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,7 @@
 angular.module('starter.services', [])
 
 .factory('particleService', function($http, $q){
-    var keys = JSON.parse(window.localStorage['sparkKeys']);
+    var keys = JSON.parse(window.localStorage['data-keys']);
     var getUrl = 'https://api.particle.io/v1/devices/' + keys.deviceId;
     return {
         getTemperature: function(){
@@ -43,46 +43,60 @@ angular.module('starter.services', [])
     };
 })
 .factory('dataService', function($http, $q) {
-    var keys = JSON.parse(window.localStorage['sparkKeys']);
-    var inputUrl = 'https://data.sparkfun.com/input/' + keys.public;
-    var getUrl = 'https://data.sparkfun.com/output/' + keys.public + '.json';
+    var keys = JSON.parse(window.localStorage['data-keys']);
+    
+    var connect = new Connect({
+        projectId: keys.connectio.projectId,
+        apiKey: keys.connectio.apiKey
+    });
     
     return {
-        all: function(){
-        },
         save: function(parameters){
             var deferred = $q.defer();
-            //var xsrf = JSON.stringify(parameters);
-            var values = transform(parameters);
-            var url = inputUrl + '?callback=JSON_CALLBACK&private_key=' + keys.private + '&' + values;
-            $http.jsonp(url)
-                .success(function(data) {
-                    if(data.success){
-                        deferred.resolve(data)
-                    }
-                    else{
-                        deferred.reject('Request failed: ' + data.message);
-                    }
-                }).error(function(data, status, headers, config) {
-                    deferred.reject('Request failed: ' + status);
-                });
-            return deferred.promise;
-        },
-        get: function(parameter){
-        },
-        getHistory: function(days){
-            var deferred = $q.defer();
-            var now = new Date();
-            var searchDate = now.setMinutes(now.getMinutes() - days * 60 * 24);
-            $http.get(getUrl + '?gte[date]=' + searchDate)
-            .success(function (data) {   
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                deferred.reject('Request failed: ' + status);
-            });
-            return deferred.promise;
+            connect.push('parameters', parameters);
         }
     };
+    
+//    var keys = JSON.parse(window.localStorage['sparkKeys']);
+//    var inputUrl = 'https://data.sparkfun.com/input/' + keys.public;
+//    var getUrl = 'https://data.sparkfun.com/output/' + keys.public + '.json';
+//    
+//    return {
+//        all: function(){
+//        },
+//        save: function(parameters){
+//            var deferred = $q.defer();
+//            //var xsrf = JSON.stringify(parameters);
+//            var values = transform(parameters);
+//            var url = inputUrl + '?callback=JSON_CALLBACK&private_key=' + keys.private + '&' + values;
+//            $http.jsonp(url)
+//                .success(function(data) {
+//                    if(data.success){
+//                        deferred.resolve(data)
+//                    }
+//                    else{
+//                        deferred.reject('Request failed: ' + data.message);
+//                    }
+//                }).error(function(data, status, headers, config) {
+//                    deferred.reject('Request failed: ' + status);
+//                });
+//            return deferred.promise;
+//        },
+//        get: function(parameter){
+//        },
+//        getHistory: function(days){
+//            var deferred = $q.defer();
+//            var now = new Date();
+//            var searchDate = now.setMinutes(now.getMinutes() - days * 60 * 24);
+//            $http.get(getUrl + '?gte[date]=' + searchDate)
+//            .success(function (data) {   
+//                deferred.resolve(data);
+//            }).error(function (data, status, headers, config) {
+//                deferred.reject('Request failed: ' + status);
+//            });
+//            return deferred.promise;
+//        }
+//    };
     
     function transform(json) {
         var str = [];
